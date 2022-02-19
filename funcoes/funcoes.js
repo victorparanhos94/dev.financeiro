4ada411784fbc7ce3c6c997d920116ffe8348f9a
Victor Paranhos <victor_paranhos@hotmail.com>
2022-02-19 02:27:27 -0200

    Funcões

--- funcoes/funcoes.js
+++ funcoes/funcoes.js
@@ -1,10 +1,12 @@
-Modal = {
+let Modal = {
     open(){
         //Abrir modal
         //Adicionar a class active ao modal
+        document.querySelector('.modal-overlay').classList.add('active')
     },
     close() {
         //Fechar modal
         //Remover a class active ao modal
+        document.querySelector('.modal-overlay').classList.remove('active')
     }
 }
